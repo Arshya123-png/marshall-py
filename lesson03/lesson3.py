95b6761fb9eb7cf6643a62b4334d29cb9fbd6b0f
Arshya Alizadeh <archyalizadeh@218X0MBLCB2W2Z1.EDU.YRDSB.NET>
2025-02-14 14:00:03 -0500

    solved lesson 3

--- lesson03/lesson3.py
+++ lesson03/lesson3.py
@@ -1,1 +1,14 @@
 # Lesson 3
+tiles = input("Enter number of tiles: ")
+#imput always a string
+tiles = int(tiles)
+
+# Square root is just the exponent of a half
+calculation = int((tiles ** 0.5) // 1)
+
+print(f"The maximum sidelenght is:  {calculation}")
+# or 
+# import math 
+# calculations = math.sqrt(tiles)
+
+print (f"Number of tiles: {tiles}")
