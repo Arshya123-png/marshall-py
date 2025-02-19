1d5d72a0d474c9d7f8c6dbd8d49b911f505e6bee
Arshya Alizadeh <archyalizadeh@218r0mbl2278rt1.edu.yrdsb.net>
2025-02-19 14:12:42 -0500

    solved lesson 4

--- lesson04/lesson4.py
+++ lesson04/lesson4.py
@@ -1,1 +1,19 @@
 # Lesson 4
+
+from math import ceil
+
+section1 = input("Enter section 1: ")
+section2 = input("Enter section 2: ")
+section3 = input("Enter section 3: ")
+
+cans = len(section1) + len(section2) + len(section3) 
+
+boxes = ceil(cans / 12)
+
+leftover = 12*boxes - cans 
+
+total_cost = 14.95 * boxes
+
+print(f"We needed {cans} pain cans.")
+print(f"We have {leftover} leftover. ")
+print(f"The project costs ${total_cost}")
